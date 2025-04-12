6f34edb281a7f21fb30f3cfe4dbdaeeb27a47dd4
danny3819 <danny.nada.djnnr@gmail.com>
2025-04-12 00:46:01 -0600

    haciendo mas cosas en 2025

--- pagina1/script.js
+++ pagina1/script.js
@@ -1,7 +1,8 @@
 let $tarjetas = document.querySelector('.contenedor-2');
 let $todo = document.querySelector(".body")
 let clonado = $tarjetas.cloneNode(true);
-!function(){
+
+!function(){ //funcion auto ejecutable que clona y pone un fondo de pantalla
   let estilo = $todo.appendChild(clonado);
-    estilo.style.backgroundImage = 'url("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg")'
+    estilo.style.backgroundImage = 'url("https://wallpapers.com/images/hd/minecraft-cloudy-sunrise-nature-xpc7uzwsfbga70xp.jpg")'
 }()
